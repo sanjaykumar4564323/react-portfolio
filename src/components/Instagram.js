c855068c0e4890f97e377d4be378c5297f5c1434
sanjaykumar4564323 <86568244+sanjaykumar4564323@users.noreply.github.com>
2021-08-08 15:26:14 -0400

    Update Instagram.js

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -11,6 +11,7 @@ const Instagram = () => {
       <div className="row spacing">
         <div className="col">
           <iframe
+            title="facebook1"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjazzysinghrealtor%2Fphotos%2Fa.2351656188382139%2F2996813230533095%2F&show_text=true&width=500"
             className="facebook1"
             scrolling="no"
@@ -20,6 +21,7 @@ const Instagram = () => {
         </div>
         <div className="col">
           <iframe
+            title="facebook2"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjazzysinghrealtor%2Fphotos%2Fa.2351656188382139%2F2994857120728706%2F&show_text=true&width=500"
             className="facebook2"
             scrolling="no"
@@ -31,6 +33,7 @@ const Instagram = () => {
       <div className="row">
         <div className="col">
           <iframe
+            title="facebook3"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjazzysinghrealtor%2Fphotos%2Fa.2351656188382139%2F2980053822209036%2F&show_text=true&width=500"
             className="facebook3"
             scrolling="no"
@@ -40,6 +43,7 @@ const Instagram = () => {
         </div>
         <div className="col">
           <iframe
+            title="facebook4"
             src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjazzysinghrealtor%2Fphotos%2Fa.2351656188382139%2F2969047636642988%2F&show_text=true&width=500"
             className="facebook4"
             scrolling="no"
