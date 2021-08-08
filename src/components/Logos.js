266e7a0bc524df4f56c21f33164c05d2aa75b56e
sanjaykumar4564323 <86568244+sanjaykumar4564323@users.noreply.github.com>
2021-08-08 15:27:50 -0400

    Update Logos.js

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -8,10 +8,10 @@ const Logos = () => {
         <div className="col"></div>
         <div className="col"></div>
         <div className="col">
-          <img className="appear small" src="https://i.imgur.com/75fHSmf.png" />
+          <img className="appear small" src="https://i.imgur.com/75fHSmf.png" alt="img" />
         </div>
         <div className="col">
-          <img className="appear small" src="https://i.imgur.com/gWvx2dR.png" />
+          <img className="appear small" src="https://i.imgur.com/gWvx2dR.png" alt="img"/>
         </div>
         <div className="col"></div>
       </div>
@@ -19,18 +19,18 @@ const Logos = () => {
         <div className="col middle medium">
           <img
             className="appear-med right-middle"
-            src="https://i.imgur.com/GmJNjSu.png"
+            src="https://i.imgur.com/GmJNjSu.png" alt="img"
           />
         </div>
         <div className="col middle small">
-          <img className="appear" src="https://i.imgur.com/hUFl7ji.png" />
+          <img className="appear" src="https://i.imgur.com/hUFl7ji.png" alt="img"/>
         </div>
       </div>
       <div className="row">
         <div className="col middle small">
           <img
             className="appear right-middle"
-            src="https://i.imgur.com/XlrSKdB.png"
+            src="https://i.imgur.com/XlrSKdB.png" alt="img"
           />
         </div>
         <div className="col-6">
@@ -39,7 +39,7 @@ const Logos = () => {
           </h1>
         </div>
         <div className="col biggest">
-          <img className="appear-main" src="https://i.imgur.com/kJKQeTR.png" />
+          <img className="appear-main" src="https://i.imgur.com/kJKQeTR.png" alt="img"/>
         </div>
       </div>
       <div className="row">
@@ -47,19 +47,19 @@ const Logos = () => {
         <div className="col">
           <img
             className="appear-med medium"
-            src="https://i.imgur.com/raZUb6D.png"
+            src="https://i.imgur.com/raZUb6D.png" alt="img"
           />
         </div>
         <div className="col">
           <img
             className="appear-main biggest"
-            src="https://i.imgur.com/u8rRmKU.png"
+            src="https://i.imgur.com/u8rRmKU.png" alt="img"
           />
         </div>
         <div className="col">
           <img
             className="appear-main medium"
-            src="https://i.imgur.com/zWzwY2J.png"
+            src="https://i.imgur.com/zWzwY2J.png" alt="img"
           />
         </div>
         <div className="col"></div>
